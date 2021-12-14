c9f5bfa3a6376c88bad7e924f23b757a9ca38e72
larinsi <larinsi15@gmail.com>
2021-12-14 13:37:01 +0300

    Task #41 in progress

--- Training_on_algorithms_1.0/41.Style_clothes.py
+++ Training_on_algorithms_1.0/41.Style_clothes.py
@@ -1,19 +1,12 @@
 import random
 
 def style(n, tshirts, m, pants):
-    best_tshirt = tshirts[0]
-    best_pants = pants[0]
+    i = j = 0
+    best_tshirt = tshirts[i]
+    best_pants = pants[j]
     best_diff = abs(best_tshirt - best_pants)
-    j = 0
-    for i in range(n):
-        while j < m - 1:
-            if best_diff == 0:
-                return best_tshirt, best_pants
-            j += 1
-            if best_diff >= abs(tshirts[i] - pants[j]):
-                best_diff = abs(tshirts[i] - pants[j])
-                best_tshirt = tshirts[i]
-                best_pants = pants[j]
+    for k in range(n + m):
+
     return best_tshirt, best_pants, best_diff
 
 
@@ -31,25 +24,36 @@ def style_naiv(n, tshirts, m, pants):
 
 
 def test_functions():
-    n = random.randint(1, 50)
-    m = random.randint(1, 50)
-    tshirts = sorted([random.randint(1, 100000) for _ in range(n)])
-    pants = sorted([random.randint(1, 100000) for _ in range(m)])
+    max_size_of_array = 20
+    max_size_of_color = 50
+    n = random.randint(1, max_size_of_array)
+    m = random.randint(1, max_size_of_array)
+    tshirts_set = set([random.randint(1, max_size_of_color) for _ in range(n)])
+    pants_set = set([random.randint(1, max_size_of_color) for _ in range(m)])
+    tshirts = sorted(list(tshirts_set))
+    pants = sorted(pants_set)
+    n = len(tshirts)
+    m = len(pants)
     print(*style(n, tshirts, m, pants))
     print(*style_naiv(n, tshirts, m, pants))
-    # print(n, tshirts)
-    # print(m, pants)
+    print(n, tshirts)
+    print(m, pants)
 
 
 test_functions()
 
+# tshirts = 2, 9, 11, 12, 15, 16, 17, 18
+# pants = 3, 5, 9, 10, 13, 14, 15, 16, 20
+#
+# print(*style(8, tshirts, 9, pants))
+# print(*style_naiv(8, tshirts, 9, pants))
 
 
 
-#with open("41.3.txt", "r") as file:
- #   n = int(file.readline().strip())
-  #  tshirts = list(map(int, file.readline().strip().split()))
-   # m = int(file.readline().strip())
-    #pants = list(map(int, file.readline().strip().split()))
-
-#print(*style(n, tshirts, m, pants))
+# with open("41.1.txt", "r") as file:
+#     n = int(file.readline().strip())
+#     tshirts = list(map(int, file.readline().strip().split()))
+#     m = int(file.readline().strip())
+#     pants = list(map(int, file.readline().strip().split()))
+#
+# print(*style(n, tshirts, m, pants))
