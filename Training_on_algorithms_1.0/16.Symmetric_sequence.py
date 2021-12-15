c0b22fa3b8b6920790de2bd0db0fcb5f072e4b69
larinsi <larinsi15@gmail.com>
2021-12-15 11:15:42 +0300

    Added solution for Task #16

--- Training_on_algorithms_1.0/16.Symmetric_sequence.py
+++ Training_on_algorithms_1.0/16.Symmetric_sequence.py
@@ -1,10 +1,18 @@
 # начинаем с середины
 def symmetric_sequence(n, array):
-    pass
+    max_sym_len = 0
+    for k in range(n):
+        slice = array[k:]
+        if slice == slice[::-1]:
+            max_sym_len = len(slice)
+            break
+    print(n - max_sym_len)
+    if n != max_sym_len:
+        result = array[:-max_sym_len][::-1]
+        print(" ".join(str(elem) for elem in result))
 
 
-
-with open("Tests/16.1.txt", 'r') as file:
+with open("Tests/16.3.txt", 'r') as file:
     n = int(file.readline().strip())
     array = list(map(int, file.readline().strip().split()))
 
